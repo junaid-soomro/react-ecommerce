7e491bb88a104d6501885fb9f53393055586e5cf
darklord <junaid-soomro@github.com>
2022-08-31 23:13:31 +0500

    Revert "prod-fix commmit (#2)"
    This reverts commit 362ba8b83708527927333848bb79b404cd9586e3.
    
    reverted this commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,15 +3,7 @@ import styled from "styled-components";
 import { popularProducts } from "../data";
 import { ProductItem } from "./ProductItem";
 
-//removed display property commit
-//added some more property for the prod fix
-
-const Container = styled.div`
-  flex-wrap: wrap;
-  justify-content: space-between;
-`;
 const Container = styled.div``;
-
 
 export const Products = () => {
   return (
