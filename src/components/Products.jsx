fc2e035238aebe62b43e511b31f5817c63ee8897
darklord <junaid-soomro@github.com>
2022-08-31 22:53:35 +0500

    removed padding property

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,7 +4,6 @@ import { popularProducts } from "../data";
 import { ProductItem } from "./ProductItem";
 
 const Container = styled.div`
-  padding: 20px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
