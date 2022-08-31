7f034e687288df6249d7421d016ae069dc4e451d
darklord <junaid-soomro@github.com>
2022-08-31 22:45:34 +0500

    commit no 4

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -25,7 +25,7 @@ const Info = styled.div`
 const Container = styled.div`
   flex: 1;
   margin: 5px;
-  min-width: 280px;
+  min-width: 28x;
   height: 350px;
   align-items: center;
   display: flex;
