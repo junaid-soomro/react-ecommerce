f3c441d141b188bc96a1d42446b9051e619b9009
darklord <junaid-soomro@github.com>
2022-08-31 22:48:10 +0500

    fixed absolute

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -10,7 +10,7 @@ const Info = styled.div`
   opacity: 0;
   width: 100%;
   height: 100%;
-  position: absolut;
+  position: absolute;
   top: 0;
   left: 0;
   background-color: rgba(0, 0, 0, 0.2);
