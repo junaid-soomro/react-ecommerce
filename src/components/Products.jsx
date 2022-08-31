bfdb765bd944f486eb0e5424651854964037db35
darklord <junaid-soomro@github.com>
2022-08-31 22:54:21 +0500

    remove display property

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,7 +4,6 @@ import { popularProducts } from "../data";
 import { ProductItem } from "./ProductItem";
 
 const Container = styled.div`
-  display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 `;
