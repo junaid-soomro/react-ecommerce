0ac0b696688556894f7e645c67b3f79e2260a298
darklord <junaid-soomro@github.com>
2022-08-31 22:57:26 +0500

    prod-fix commmit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { popularProducts } from "../data";
 import { ProductItem } from "./ProductItem";
 
+//removed display property commit
+//added some more property for the prod fix
+
 const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
